f1fd4c5bebe9e1e786d749f83204cdcd53b9a258
Abulatan Olawole Samuel <Abulatano@gmail.com>
2026-01-11 14:50:17 +0100

    Worked on some and responsive

--- js/codelife.js
+++ js/codelife.js
@@ -1,3 +1,10 @@
 function Myfunction (){
     alert('Login succesfully')
 }
+
+const menuBtn = document.getElementById("menuBtn");
+const nav = document.getElementById("navbar");
+
+menuBtn.addEventListener("click", () => {
+  nav.classList.toggle("show");
+});
